77913554ced65e3d2a6268cdb2dbb6951eb61717
Emiliano Bregains <ebregains@gmail.com>
2024-10-14 09:43:32 -0300

    Validation and cleaning

---
+++ chirp.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+// POST validation
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	// create struct for parameters
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	params := parameters{}
+	// decode
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+	// create struct for return values
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+	// respond
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	cleaned := getCleanedBody(params.Body, badWords)
+
+	respondWithJSON(w, http.StatusOK, returnVals{
+		CleanedBody: cleaned,
+	})
+}
+
+func getCleanedBody(body string, badWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
+}
