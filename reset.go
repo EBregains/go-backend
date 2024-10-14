a8e474ca678c9ae364a0a884c53ecf4fa661df3a
Emiliano Bregains <ebregains@gmail.com>
2024-10-14 16:01:03 -0300

    reset clean database

--- reset.go
+++ reset.go
@@ -3,7 +3,15 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Reset is only allowed on development environment."))
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
+	cfg.db.DeleteAllUsers(r.Context())
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
+	w.Write([]byte("Hits reset to 0 and database restored to initial state"))
 }
