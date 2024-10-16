ea519a22f59ec9415c9c7bfd441d38db1f67eba8
Emiliano Bregains <ebregains@gmail.com>
2024-10-16 15:53:39 -0300

    add delete chirp

--- chirp.go
+++ chirp.go
@@ -131,7 +131,7 @@ func ValidateAndCleanChirp(body string) (string, error) {
 
 	const maxChirpLength = 140
 	if len(body) > maxChirpLength {
-		return "", fmt.Errorf("The chirp exceeds maximum length: %v char", maxChirpLength)
+		return "", fmt.Errorf("the chirp exceeds maximum length: %v char", maxChirpLength)
 	}
 	// respond
 	badWords := map[string]struct{}{
@@ -153,4 +153,46 @@ func getCleanedBody(body string, badWords map[string]struct{}) string {
 	}
 	cleaned := strings.Join(words, " ")
 	return cleaned
+}
+
+// DELETE {id}
+func (cfg *apiConfig) handlerDeleteChirpByID(w http.ResponseWriter, r *http.Request) {
+	// Get Chirp ID from path
+	reqId, err := uuid.Parse(r.PathValue("chirpID"))
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Fail at parsing uuid", err)
+		return
+	}
+	// Get token from header and validate
+	headerToken, err := auth.GetBearerToken(r.Header)
+	fmt.Println(headerToken)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Error getting Authorization token from Header", err)
+		return
+	}
+	userID, err := auth.ValidateJWT(headerToken, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't validate JWT", err)
+		return
+	}
+
+	// We need to get the chirp to see its creator -- MAY GENERATE A QUERY FOR JUST GETTING THE ID --
+	chirp, err := cfg.db.GetChirpById(r.Context(), reqId)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't find the chirp with requested ID", err)
+		return
+	}
+	if userID != chirp.UserID {
+		respondWithError(w, http.StatusForbidden, "User doesn't have permissions to delete the chirp", err)
+		return
+	}
+
+	// Delete chirp from db
+	err = cfg.db.DeleteChirpById(r.Context(), reqId)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't delete chirp", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
 }
